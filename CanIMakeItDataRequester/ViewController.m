941afafe532fdce08d7efc43c51ae191369a1e66
smore9982 <sameermore524@gmail.com>
2014-03-02 02:13:00 -0500

    Initial Commit.

--- CanIMakeItDataRequester/ViewController.m
+++ CanIMakeItDataRequester/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *getDataButton;
+@property NSURLConnection *connection;
 
 @end
 
@@ -17,13 +19,72 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+
+
+- (IBAction)getData:(id)sender {
+    [self startReceive];
+    
+}
+
+- (void)startReceive
+// Starts a connection to download the current URL.
+{
+    NSString *urlStr = @"http://ec2-54-83-18-44.compute-1.amazonaws.com:8080/CanIMakeWebService/GetStops";
+    BOOL  success;
+    NSURL *url= [NSURL URLWithString:urlStr];
+    NSURLRequest *request;
+    
+    request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        NSError* error;
+        NSArray* jsonData = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        for (int i=0;i<jsonData.count; i++) {
+            NSDictionary* stopDict= [jsonData objectAtIndex:i];
+            
+            NSString *stopName = [stopDict objectForKey:@"stop_name"];
+            NSLog(stopName);
+        }
+    }];
+}
+
+- (void)connection:(NSURLConnection *)theConnection didReceiveResponse:(NSURLResponse *)response{
+    NSHTTPURLResponse * httpResponse;
+    NSString *          contentTypeHeader;
+    
+    assert(theConnection == self.connection);
+    
+    httpResponse = (NSHTTPURLResponse *) response;
+    assert( [httpResponse isKindOfClass:[NSHTTPURLResponse class]] );
+    
+
+    // -MIMEType strips any parameters, strips leading or trailer whitespace, and lower cases
+    // the string, so we can just use -isEqual: on the result.
+    contentTypeHeader = [httpResponse MIMEType];
+    NSLog(contentTypeHeader);
+
+}
+
+- (void)connection:(NSURLConnection *)theConnection didReceiveData:(NSData *)data
+{
+    NSError* error;
+    NSString *response=[[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    NSArray* jsonData = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    for (int i=0;i<jsonData.count; i++) {
+        NSDictionary* stopDict= [jsonData objectAtIndex:i];
+        
+        NSString *stopName = [stopDict objectForKey:@"stop_name"];
+        NSLog(stopName);
+    }
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)theConnection{
+    
+}
 @end
